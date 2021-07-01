73a78e55dda07928a4aab2900980d559f8f30a87
Ryan Miller <ryan.kp.miller@gmail.com>
2021-07-01 16:18:06 -0500

    Move app.init_db call to create_app function

--- api/app.py
+++ api/app.py
@@ -2,14 +2,23 @@ from flask import Flask
 from flask_restx import Api
 from flask_jwt_extended import JWTManager
 
+from api.db import db
+from api.config import app_config
 from api.resources.user import api as user_namespace
 from api.resources.favorite import api as favorite_namespace
-from api.config import app_config
 
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(app_config[config_name])
     jwt = JWTManager(app)
+    db.init_app(app)
+
+    # before any request to the API, this function will be called 
+    # and will create the data.db file and all the tables within the db (unless they already exist)
+    @app.before_first_request
+    def create_tables():
+        db.create_all()
+
     api = Api(
         app, 
         title="The Drink Almanac REST API",
--- manage.py
+++ manage.py
@@ -2,16 +2,9 @@ import os
 from api.app import create_app
 from api.db import db
 
-config_name = "testing" #os.getenv('APP_SETTINGS')
+config_name = os.getenv('APP_SETTINGS')
 app = create_app(config_name)
 
-# before any request to the API, this function will be called 
-# and will create the data.db file and all the tables within the db (unless they already exist)
-@app.before_first_request
-def create_tables():
-    db.create_all()
-
-db.init_app(app)
 
 if __name__ == "__main__":
     app.run(
