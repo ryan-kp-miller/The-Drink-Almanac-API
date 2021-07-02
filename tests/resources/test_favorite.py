73f5fe50607b5b1caf3e7d3eff879148878d86ce
Ryan Miller <ryan.kp.miller@gmail.com>
2021-07-02 14:59:46 -0500

    Add unit tests for favorite endpoint get method

--- tests/resources/test_favorite.py
+++ tests/resources/test_favorite.py
@@ -44,7 +44,7 @@ class TestFavorite:
         data = loads(response.data)
         assert 'Missing Authorization Header' in data['msg']
         
-    def test_post_incorrect_access_token(self, client):
+    def test_post_invalid_access_token(self, client):
         # access token takend from jwt.io
         bad_auth_header = {
             "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ"
@@ -63,4 +63,50 @@ class TestFavorite:
 
     def test_post_invalid_drink_id(self, client):
         response = client.post('/favorite/test', headers=self.auth_header)
+        assert response.status_code == 404
+
+    def test_get_correct_args(self, client):
+        client.post(
+            f'/favorite/{self.TEST_DRINK_ID}', 
+            headers=self.auth_header
+        )
+        response = client.get(
+            f'/favorite/{self.TEST_DRINK_ID}', 
+            headers=self.auth_header
+        )
+
+        assert response.status_code == 200
+        data = loads(response.data)
+        for key in data.keys():
+            assert key in self.EXPECTED_KEYS
+        assert data['drink_id'] == self.TEST_DRINK_ID
+        # first user/favorite created, so their id's should always be 1
+        assert data['user_id'] == 1 
+        assert data['id'] == 1  
+    
+    def test_get_missing_auth_header(self, client):
+        response = client.get(f'/favorite/{self.TEST_DRINK_ID}')
+        assert response.status_code == 401
+        data = loads(response.data)
+        assert 'Missing Authorization Header' in data['msg']
+        
+    def test_get_invalid_access_token(self, client):
+        # access token takend from jwt.io
+        bad_auth_header = {
+            "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ"
+            "zdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM"
+            "5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
+        }
+        response = client.get(
+            f"/favorite/{self.TEST_DRINK_ID}",
+            headers=bad_auth_header
+        )
+        assert response.status_code == 422
+        
+    def test_get_missing_drink_id(self, client):
+        response = client.get('/favorite/', headers=self.auth_header)
+        assert response.status_code == 404
+
+    def test_get_invalid_drink_id(self, client):
+        response = client.get('/favorite/test', headers=self.auth_header)
         assert response.status_code == 404
