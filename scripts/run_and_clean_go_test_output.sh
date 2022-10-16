f60a5c2fb8aa33a11849db58d759f2551b467b82
Ryan Miller <r.miller@fetchrewards.com>
2022-10-16 15:48:50 -0500

    Exclude stub files from code coverage as well

--- scripts/run_and_clean_go_test_output.sh
+++ scripts/run_and_clean_go_test_output.sh
@@ -1,4 +1,10 @@
 go test ./go_api/... -v -covermode=atomic -coverprofile=./go_api/coverage_raw.txt -coverpkg=./go_api/...
 
-# remove any lines with mocks in them to exclude mock files from the code coverage
-cat ./go_api/coverage_raw.txt | grep -v "mocks" > ./go_api/coverage.txt
+# join the expressions together and then remove the leading delimiter
+file_path_regexp_to_ignore=("mocks" "Stub")
+combined_regexp=$(printf "\|%s" "${file_path_regexp_to_ignore[@]}")
+combined_regexp=${combined_regexp:2}
+echo "combined expression ${combined_regexp}"
+
+# remove any lines that match one of the expressions in them from the code coverage
+cat ./go_api/coverage_raw.txt | grep -v $combined_regexp > ./go_api/coverage.txt
