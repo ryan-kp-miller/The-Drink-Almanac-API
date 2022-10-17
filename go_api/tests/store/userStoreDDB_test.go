aa6fa41146aa027059c634d2c7c65b8e2d48fb4e
Ryan Miller <r.miller@fetchrewards.com>
2022-10-16 21:18:06 -0500

    Add DDBClient mock and unit tests for UserStoreDDB methods

---
+++ go_api/tests/store/userStoreDDB_test.go
@@ -0,0 +1,230 @@
+package store
+
+import (
+	"context"
+	"fmt"
+	"reflect"
+	"strconv"
+	"testing"
+	"the-drink-almanac-api/mocks"
+	"the-drink-almanac-api/model"
+	"the-drink-almanac-api/store"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb"
+	"github.com/aws/aws-sdk-go-v2/service/dynamodb/types"
+	"github.com/stretchr/testify/mock"
+)
+
+func TestUserStoreDDB_FindAll(t *testing.T) {
+	numUsers := 5
+	userItems := make([]map[string]types.AttributeValue, numUsers)
+	mockUsers := make([]model.User, numUsers)
+	for i := 0; i < numUsers; i++ {
+		iStr := strconv.Itoa(i)
+		userItems[i] = map[string]types.AttributeValue{
+			"id":       &types.AttributeValueMemberS{Value: iStr},
+			"username": &types.AttributeValueMemberS{Value: iStr},
+			"password": &types.AttributeValueMemberS{Value: iStr},
+		}
+		mockUsers[i] = model.User{
+			Id:       iStr,
+			Username: iStr,
+			Password: iStr,
+		}
+	}
+	scanOutput := &dynamodb.ScanOutput{
+		Count:        int32(numUsers),
+		Items:        userItems,
+		ScannedCount: int32(numUsers),
+	}
+	scanInput := &dynamodb.ScanInput{
+		TableName: aws.String("the-drink-almanac-users"),
+	}
+	tests := []struct {
+		name          string
+		expectedUsers []model.User
+		returnedError error
+		expectError   bool
+	}{
+		{
+			name:          "Successfully retrieve users",
+			expectedUsers: mockUsers,
+			returnedError: nil,
+			expectError:   false,
+		},
+		{
+			name:          "Failed to retrieve users",
+			expectedUsers: nil,
+			returnedError: fmt.Errorf("failed to retrieve users"),
+			expectError:   true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			mockDdbClient := mocks.NewDDBClient(t)
+			mockDdbClient.On("Scan", context.TODO(), scanInput).Return(scanOutput, tt.returnedError)
+			userStore := store.UserStoreDDB{DynamodbClient: mockDdbClient}
+			got, err := userStore.FindAll()
+			if (err != nil) != tt.expectError {
+				t.Errorf("UserStoreDDB.FindAll() error = %v", err)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.expectedUsers) {
+				t.Errorf("UserStoreDDB.FindAll() = %v, want %v", got, tt.expectedUsers)
+			}
+		})
+	}
+}
+
+func TestUserStoreDDB_FindUserByUsername(t *testing.T) {
+	numUsers := 5
+	userItems := make([]map[string]types.AttributeValue, numUsers)
+	mockUsers := make([]model.User, numUsers)
+	for i := 0; i < numUsers; i++ {
+		iStr := strconv.Itoa(i)
+		userItems[i] = map[string]types.AttributeValue{
+			"id":       &types.AttributeValueMemberS{Value: iStr},
+			"username": &types.AttributeValueMemberS{Value: "0"},
+			"password": &types.AttributeValueMemberS{Value: iStr},
+		}
+		mockUsers[i] = model.User{
+			Id:       iStr,
+			Username: "0",
+			Password: iStr,
+		}
+	}
+	scanOutput := &dynamodb.ScanOutput{
+		Count:        int32(numUsers),
+		Items:        userItems,
+		ScannedCount: int32(numUsers),
+	}
+	tests := []struct {
+		name          string
+		expectedUsers []model.User
+		returnedError error
+		expectError   bool
+	}{
+		{
+			name:          "Successfully retrieve users",
+			expectedUsers: mockUsers,
+			returnedError: nil,
+			expectError:   false,
+		},
+		{
+			name:          "Failed to retrieve users",
+			expectedUsers: nil,
+			returnedError: fmt.Errorf("failed to retrieve users"),
+			expectError:   true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			mockDdbClient := mocks.NewDDBClient(t)
+			mockDdbClient.On("Scan", context.TODO(), mock.AnythingOfType("*dynamodb.ScanInput")).Return(scanOutput, tt.returnedError)
+			userStore := store.UserStoreDDB{DynamodbClient: mockDdbClient}
+			got, err := userStore.FindAll()
+			if (err != nil) != tt.expectError {
+				t.Errorf("UserStoreDDB.FindAll() error = %v", err)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.expectedUsers) {
+				t.Errorf("UserStoreDDB.FindAll() = %v, want %v", got, tt.expectedUsers)
+			}
+		})
+	}
+}
+
+func TestUserStoreDDB_CreateNewUser(t *testing.T) {
+	mockUser := model.User{
+		Id:       "0",
+		Username: "0",
+		Password: "0",
+	}
+	putItemInput := &dynamodb.PutItemInput{
+		TableName: aws.String("the-drink-almanac-users"),
+		Item: map[string]types.AttributeValue{
+			"id":       &types.AttributeValueMemberS{Value: mockUser.Id},
+			"username": &types.AttributeValueMemberS{Value: mockUser.Username},
+			"password": &types.AttributeValueMemberS{Value: mockUser.Password},
+		},
+	}
+	putItemOutput := &dynamodb.PutItemOutput{}
+	tests := []struct {
+		name          string
+		expectedUser  model.User
+		returnedError error
+		expectError   bool
+	}{
+		{
+			name:          "Successfully created a user",
+			expectedUser:  mockUser,
+			returnedError: nil,
+			expectError:   false,
+		},
+		{
+			name:          "Failed to create a user",
+			expectedUser:  mockUser,
+			returnedError: fmt.Errorf("failed to create the user"),
+			expectError:   true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			mockDdbClient := mocks.NewDDBClient(t)
+			mockDdbClient.On("PutItem", context.TODO(), putItemInput).Return(putItemOutput, tt.returnedError)
+			userStore := store.UserStoreDDB{DynamodbClient: mockDdbClient}
+			err := userStore.CreateNewUser(tt.expectedUser)
+			if (err != nil) != tt.expectError {
+				t.Errorf("UserStoreDDB.FindAll() error = %v", err)
+				return
+			}
+		})
+	}
+}
+
+func TestUserStoreDDB_DeleteUser(t *testing.T) {
+	mockUser := model.User{
+		Id:       "0",
+		Username: "0",
+		Password: "0",
+	}
+	deleteItemInput := &dynamodb.DeleteItemInput{
+		TableName: aws.String("the-drink-almanac-users"),
+		Key: map[string]types.AttributeValue{
+			"id": &types.AttributeValueMemberS{Value: mockUser.Id},
+		},
+	}
+	deleteItemOutput := &dynamodb.DeleteItemOutput{}
+	tests := []struct {
+		name          string
+		expectedUser  model.User
+		returnedError error
+		expectError   bool
+	}{
+		{
+			name:          "Successfully deleted the user",
+			expectedUser:  mockUser,
+			returnedError: nil,
+			expectError:   false,
+		},
+		{
+			name:          "Failed to create a user",
+			expectedUser:  mockUser,
+			returnedError: fmt.Errorf("failed to delete the user"),
+			expectError:   true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			mockDdbClient := mocks.NewDDBClient(t)
+			mockDdbClient.On("DeleteItem", context.TODO(), deleteItemInput).Return(deleteItemOutput, tt.returnedError)
+			userStore := store.UserStoreDDB{DynamodbClient: mockDdbClient}
+			err := userStore.DeleteUser(tt.expectedUser.Id)
+			if (err != nil) != tt.expectError {
+				t.Errorf("UserStoreDDB.FindAll() error = %v", err)
+				return
+			}
+		})
+	}
+}
