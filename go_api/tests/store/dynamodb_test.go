eef441c901ab13d2c6f9a8deb535a59326e46092
Ryan Miller <r.miller@fetchrewards.com>
2022-10-16 20:04:06 -0500

    Add unit tests for DefaultEnv

---
+++ go_api/tests/store/dynamodb_test.go
@@ -0,0 +1,48 @@
+package store
+
+import (
+	"testing"
+	"the-drink-almanac-api/store"
+)
+
+func TestDefaultEnv(t *testing.T) {
+	type args struct {
+		envVarName   string
+		defaultValue string
+	}
+	tests := []struct {
+		name         string
+		args         args
+		want         string
+		shouldSetEnv bool
+	}{
+		{
+			name: "Env variable found",
+			args: args{
+				envVarName:   "AWS_ENDPOINT",
+				defaultValue: "default",
+			},
+			want:         "test_aws_endpoint",
+			shouldSetEnv: true,
+		},
+		{
+			name: "Env variable not found",
+			args: args{
+				envVarName:   "AWS_ENDPOINT",
+				defaultValue: "default",
+			},
+			want:         "default",
+			shouldSetEnv: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if tt.shouldSetEnv {
+				t.Setenv(tt.args.envVarName, tt.want)
+			}
+			if got := store.DefaultEnv(tt.args.envVarName, tt.args.defaultValue); got != tt.want {
+				t.Errorf("DefaultEnv() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
