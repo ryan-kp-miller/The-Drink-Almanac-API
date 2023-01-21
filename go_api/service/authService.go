ea5776fbe3a7b7e7fd8094736fd1e87565bf3d5e
Ryan Miller <r.miller@fetchrewards.com>
2023-01-21 14:48:48 -0600

    Add JwtAuthService to handle user authentication

---
+++ go_api/service/authService.go
@@ -0,0 +1,63 @@
+package service
+
+import (
+	"os"
+	"the-drink-almanac-api/appErrors"
+	"time"
+
+	"github.com/golang-jwt/jwt"
+)
+
+type AuthService interface {
+	CreateNewToken(userId string, ttlMinutes int) (string, error)
+	ValidateToken(string) (string, error)
+}
+
+type JwtAuthService struct {
+	authSecretKey []byte
+}
+
+// CreateNewToken generates a new token with the provided userId stored in it and an expiry based on the provided number of minutes
+func (s JwtAuthService) CreateNewToken(userId string, expiryDurationMinutes int) (string, error) {
+	token := jwt.New(jwt.SigningMethodHS256)
+	claims := token.Claims.(jwt.MapClaims)
+	claims["exp"] = time.Now().Add(time.Duration(expiryDurationMinutes) * time.Minute)
+	claims["authorized"] = true
+	claims["userId"] = userId
+
+	tokenString, err := token.SignedString(s.authSecretKey)
+	if err != nil {
+		return "", err
+	}
+
+	return tokenString, nil
+}
+
+// ValidateToken verifies that the token is valid and returns the userId if it's valid
+func (s JwtAuthService) ValidateToken(tokenString string) (string, error) {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		_, ok := token.Method.(*jwt.SigningMethodHMAC)
+		if !ok {
+			return nil, appErrors.NewInvalidAuthTokenError("invalid token format")
+		}
+		return token, nil
+	})
+	if err != nil {
+		return "", err
+	}
+
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if ok && token.Valid {
+		userId := claims["userId"].(string)
+		return userId, nil
+	} else {
+		return "", appErrors.NewInvalidAuthTokenError("token is no longer valid")
+	}
+}
+
+func NewJwtAuthService() JwtAuthService {
+	authSecretKey := os.Getenv("JWT_SECRET_KEY")
+	return JwtAuthService{
+		authSecretKey: []byte(authSecretKey),
+	}
+}
